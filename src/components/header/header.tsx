9595d94aa07e48513051d28ef3147022d5cffe96
Marty Mitchell <martyandmojo@icloud.com>
2025-04-26 18:05:12 -0700

    Fix header border displaying on Safari when overscrolling

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -22,7 +22,7 @@ export function Header() {
   return (
     <header
       className={`fixed top-0 left-0 right-0 z-50 bg-background text-foreground h-[102px] transition-all duration-150 border-b-2 ${
-        scrollPosition ? "border-lavender-500" : "border-background"
+        scrollPosition > 0 ? "border-lavender-500" : "border-background"
       }`}
     >
       <div className="flex items-center justify-between max-w-screen-2xl mx-auto px-6 md:px-12 py-8">
