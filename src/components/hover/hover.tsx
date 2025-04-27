102232b57e44be818e976f4ee13cecbb0a32e992
Marty Mitchell <martyandmojo@icloud.com>
2025-04-26 19:19:07 -0700

    Complete `Hover` component

--- src/components/hover/hover.tsx
+++ src/components/hover/hover.tsx
@@ -1,13 +1,37 @@
-import { ReactNode } from "react";
+"use client";
+
+import { ReactNode, useCallback, useState } from "react";
+import { Text } from "../text";
 
 type HoverProps = {
   children: ReactNode;
+  content: ReactNode;
 };
 
-export function Hover({ children }: HoverProps) {
+export function Hover({ children, content }: HoverProps) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = useCallback(() => setIsHovered(true), []);
+  const handleMouseLeave = useCallback(() => setIsHovered(false), []);
+
   return (
-    <div className="absolute -top-1 left-1/2 whitespace-nowrap -translate-x-1/2 -translate-y-full bg-gray-500 px-2 py-1 rounded-md">
+    <div
+      className="relative"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       {children}
+      <div
+        className={`absolute ${
+          isHovered ? "opacity-100" : "opacity-0"
+        } -top-1 left-1/2 whitespace-nowrap -translate-x-1/2 -translate-y-full bg-gray-500 px-2 py-1 rounded-md transition-opacity`}
+      >
+        {typeof content === "string" ? (
+          <Text variant="body2">{content}</Text>
+        ) : (
+          content
+        )}
+      </div>
     </div>
   );
 }
