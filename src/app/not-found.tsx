5c7ce1e1b66086b123b12f374f84841ee4ea3df7
Marty Mitchell <martyandmojo@icloud.com>
2025-04-27 16:16:56 -0700

    Add custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/button";
+import { Text } from "@/components/text";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col gap-4 items-center">
+      <Text variant="h3">404 - Page Not Found</Text>
+      <Text variant="body1">
+        The page you are looking for does not exist or has been moved.
+      </Text>
+      <Link className="w-full max-w-64" href="/">
+        <Button className="w-full mt-8">Go to Home</Button>
+      </Link>
+    </div>
+  );
+}
